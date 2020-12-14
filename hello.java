52d0ea78a14f13dd06384c616682f8d8053c5aee
kawser <kawserahmed102030@gmail.com>
2020-12-14 21:37:54 +0600

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(string[] args) {
         system.out.printIn("Vs code is Awesome");
+
+        greeting("KAWSER AHMED")
+    }
+
+    public static void greeting(String name) {
+        System.out.printIn("Welcome " + name);
     }
 }
